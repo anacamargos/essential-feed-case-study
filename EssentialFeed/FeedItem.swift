4cf53a884dbe4023c0ba718e2d4b4bea3752790e
Ana Leticia Camargos <alcvlara@sga.pucminas.br>
2020-02-21 08:51:19 -0300

    Create EssentialFeed framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,9 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ana Leticia Camargos on 21/02/20.
+//  Copyright © 2020 Ana Leticia Camargos. All rights reserved.
+//
+
+import Foundation
