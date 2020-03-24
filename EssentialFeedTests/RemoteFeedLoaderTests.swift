2389653c7c1de2569447b9dd3d7fa5df9bf5aeb6
Ana Camargos <alcvlara@sga.pucminas.br>
2020-03-24 09:50:13 -0300

    Delivers empty items array pn 200 HTTP Response with JSON empty list

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -74,6 +74,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load {
+            capturedResults.append($0)
+        }
+        
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        XCTAssertEqual(capturedResults, [.success([])])
+            
+    }
+    
     // MARK: - Testing Helpers
     
     private func makeSUT(
