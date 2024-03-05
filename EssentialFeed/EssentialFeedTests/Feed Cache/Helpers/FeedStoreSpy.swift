678b73ca64a641742671da1264273d50470ffa07
anacamargos <anacamargosvl@gmail.com>
2024-03-05 19:18:52 -0300

    Extract FeedCacheSpy into a shared scope to remove duplication

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,48 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Ana Leticia Camargos on 05/03/24.
+//
+
+import Foundation
+import EssentialFeed
+
+final class FeedStoreSpy: FeedStore {
+        
+    enum ReceivedMessage: Equatable {
+        case deleteCachedFeed
+        case insert([LocalFeedImage], Date)
+    }
+    
+    var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [Completion]()
+    private var insertCompletions = [Completion]()
+    
+    func deleteCachedFeed(completion: @escaping Completion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping Completion) {
+        insertCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = .zero) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = .zero) {
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = .zero) {
+        insertCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = .zero) {
+        insertCompletions[index](nil)
+    }
+}
